82a647a466111aa85388e11c7b045dc88f092e6b
Adam Szymajda <adam.szymajda@eu.equinix.com>
2020-10-29 14:59:16 +0100

    Init commit

---
+++ mod9task1.py
@@ -0,0 +1,43 @@
+import requests
+import csv
+from flask import Flask, render_template, request, redirect
+
+response = requests.get("http://api.nbp.pl/api/exchangerates/tables/C?format=json")
+
+
+data = response.json()
+
+app = Flask(__name__)
+
+def create_csv():
+    with open("xe.csv", mode="w") as f:
+        fieldnames = ['currency', 'code', 'bid', 'ask']
+        writer = csv.DictWriter(f, delimiter=';', fieldnames=fieldnames)
+        for currency in data[0]['rates']:
+            writer.writerow(currency)
+
+
+@app.route("/xe/", methods=['GET', 'POST'])
+def currency_exchange():
+    if request.method == "GET":
+        tab = 'Dane'
+        select_data = [x['code'] for x in data[0]['rates']]
+        return render_template('xe.html', currency_list=select_data, tab=tab)
+    if request.method == 'POST':
+        qty = int(request.form['qty'])
+        select = request.form.get('currency')
+        for currency in data[0]['rates']:
+            if currency['code'] == select:
+                return f"Należność w PLN: {qty*currency['ask']}"
+        pln_to_pay = qty
+    return redirect("/xe")
+
+
+
+
+def main():
+    create_csv()
+
+if __name__ == "__main__":
+    main()
+    app.run(debug=True)
