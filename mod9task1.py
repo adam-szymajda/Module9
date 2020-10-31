5afea552499797320b73581c08dd7c3f1a015738
Adam Szymajda <adam.szymajda@eu.equinix.com>
2020-10-31 20:19:52 +0100

    Fixed few bugs

--- mod9task1.py
+++ mod9task1.py
@@ -2,6 +2,7 @@ import requests
 import csv
 from flask import Flask, render_template, request, redirect
 
+
 response = requests.get("http://api.nbp.pl/api/exchangerates/tables/C?format=json")
 
 
@@ -24,11 +25,14 @@ def currency_exchange():
         select_data = [x['code'] for x in data[0]['rates']]
         return render_template('xe.html', currency_list=select_data, tab=tab)
     if request.method == 'POST':
-        qty = int(request.form['qty'])
-        select = request.form.get('currency')
-        for currency in data[0]['rates']:
-            if currency['code'] == select:
-                return f"Należność w PLN: {qty*currency['ask']}"
+        try:
+            qty = float(request.form['qty'])
+            select = request.form.get('currency')
+            for currency in data[0]['rates']:
+                if currency['code'] == select:
+                    return f"Należność w PLN: {qty*currency['ask']}"
+        except:
+            return "Incorrect value"
     return redirect("/xe")
 
 
