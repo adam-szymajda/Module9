f6380b91db31abaed0229e4323172f8e833a8fbe
Adam Szymajda <adam.szymajda@eu.equinix.com>
2020-11-04 12:31:28 +0100

    Cache as decorator - problem

--- mod9task1.py
+++ mod9task1.py
@@ -10,6 +10,10 @@ from flask import Flask, render_template, request, redirect
 
 app = Flask(__name__)
 
+CACHE_FOLDER = "cache"
+CACHE_FILE = "xe.pickle"
+ENDPOINT = "http://api.nbp.pl/api/exchangerates/tables/C?format=json"
+
 # def create_csv():
 #     with open("xe.csv", mode="w") as f:
 #         fieldnames = ['currency', 'code', 'bid', 'ask']
@@ -51,12 +55,36 @@ def load_data(filename, folder):
 
 # data = load_data("xe.pickle", "cache")
 
+def cache_data(f):
+    def wrapper(*args, **kwargs):
+        path = os.path.join(CACHE_FOLDER, CACHE_FILE)
+        try:
+            print("trying......")
+            with open(path, 'rb') as cache:
+                rates, timestamp = pickle.load(cache)
+                print(rates.keys(), timestamp, time.time() - timestamp)
+            if time.time() - timestamp > 30:
+                raise IOError()
+        except IOError:
+            rates = f()
+            print("Fetching from API")
+            with open(path, 'wb') as cache:
+                pickle.dump((rates, time.time()), cache)
+        return rates
+    return wrapper()
 
+# @cache_data
+def rate():
+    rates = {
+        rate['code']: float(rate['ask']) for rate in requests.get(ENDPOINT).json()[0]['rates']
+    }
+    return rates
 
 @app.route("/xe/", methods=['GET', 'POST'])
 def currency_exchange():
     errors = []
-    select_data = [x['code'] for x in data[0]['rates']]
+    select_data = rate().keys()
+    print(select_data)
     if request.method == "GET":
         tab = 'Dane'
         return render_template('xe.html', currency_list=select_data, tab=tab)
@@ -64,10 +92,10 @@ def currency_exchange():
         try:
             qty = float(request.form['qty'])
             select = request.form.get('currency')
-            for currency in data[0]['rates']:
-                if currency['code'] == select:
+            # for currency in data[0]['rates']:
+            #     if currency['code'] == select:
                     # return f"Należność w PLN: {qty*currency['ask']}"
-                    return render_template('xe.html', currency_list=select_data, errors=errors, tab='Do zapłaty', to_pay=qty*currency['ask'])
+            return render_template('xe.html', currency_list=select_data, errors=errors, tab='Do zapłaty', to_pay=qty*rate()[select])
         except:
             errors.append("Incorrect value")
             return render_template('xe.html', currency_list=select_data, errors=errors, tab='Error')
