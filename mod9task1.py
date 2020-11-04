1da7431ea049d2358fb36d77eb40839b9ba14425
Adam Szymajda <adam.szymajda@eu.equinix.com>
2020-11-04 12:32:49 +0100

    Cache as decorator - problem

--- mod9task1.py
+++ mod9task1.py
@@ -21,36 +21,6 @@ ENDPOINT = "http://api.nbp.pl/api/exchangerates/tables/C?format=json"
 #         for currency in data[0]['rates']:
 #             writer.writerow(currency)
 
-def create_pickle_cache(data, filename):
-    with open(filename, mode="wb") as f:
-        pickle.dump(data, f)
-
-def read_pickle_cache(path):
-    with open(path, mode="rb") as f:
-        content = pickle.load(f)
-    return content
-
-def clear_cache(filename, folder):
-    path = os.path.join(folder, filename)
-    # if path and (time.time() - os.path.getatime(path) > limit * 60):
-    #     print("Clearing cache")
-    #     os.remove(path)
-    print("Clearing cache")
-    os.remove(path)
-
-def load_data(filename, folder):
-    path = os.path.join(folder, filename)
-    if not os.path.exists(path):
-        print("No cache, fetching from API")
-        response = requests.get("http://api.nbp.pl/api/exchangerates/tables/C?format=json")
-        api_output = response.json()
-        create_pickle_cache(api_output, path)
-        return api_output
-    else:
-        print("Fetching cached data")
-        return read_pickle_cache(path)
-
-
 
 
 # data = load_data("xe.pickle", "cache")
